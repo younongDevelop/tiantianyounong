102855e0caffcb5b8744bdef56787c5a987f34e9
weixiangbin <317505615@qq.com>
2015-09-29 17:28:09 +0800

    fix 管理员

--- younon/public/js/adminstr/adminSer.js
+++ younon/public/js/adminstr/adminSer.js
@@ -3,51 +3,36 @@
  */
 angular.module('admin.services', [])
 
-    .factory('admin', function() {
-        // Might use a resource here that returns a JSON array
+    .factory('adminGetOrders', function($http) {
+            var orders=[];
+            var orderStatue='';
 
-        // Some fake testing data
-        var chats = [{
-            id: 0,
-            name: 'Ben Sparrow',
-            lastText: 'You on your way?',
-            face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-        }, {
-            id: 1,
-            name: 'Max Lynx',
-            lastText: 'Hey, it\'s me',
-            face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-        }, {
-            id: 2,
-            name: 'Adam Bradleyson',
-            lastText: 'I should buy a boat',
-            face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-        }, {
-            id: 3,
-            name: 'Perry Governor',
-            lastText: 'Look at my mukluks!',
-            face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-        }, {
-            id: 4,
-            name: 'Mike Harrington',
-            lastText: 'This is wicked good ice cream.',
-            face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-        }];
-
-        return {
-            all: function() {
-                return chats;
+        return{
+            getOrders:function(cb){
+                cb(orders);
             },
-            remove: function(chat) {
-                chats.splice(chats.indexOf(chat), 1);
-            },
-            get: function(chatId) {
-                for (var i = 0; i < chats.length; i++) {
-                    if (chats[i].id === parseInt(chatId)) {
-                        return chats[i];
-                    }
+            loadOrdersArr:function(statue,page,size,cb){
+                if(!orderStatue){
+                    orderStatue=statue;
+                }else if(orderStatue!=statue){
+                    console.log('sdfsdfsdf');
+                    console.log(orderStatue);
+                    console.log(orders);
+                    orderStatue=statue;
+                    orders=[];
                 }
-                return null;
+
+                $http.get('/adminOrders/'+statue+'/'+page+'/'+size).success(function(data){
+                    data.forEach(function(item){
+                        orders.push(item);
+                    })
+                    console.log(orders);
+                    cb(data);
+                }).error(function(res){
+                    console.log(res);
+                });
             }
-        };
+
+        }
+
     });
