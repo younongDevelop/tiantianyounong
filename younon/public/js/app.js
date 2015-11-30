3b7ba9b97a3a123984fe2e422816ba4aaef86d85
weixiangbin <317505615@qq.com>
2015-11-30 16:02:08 +0800

    fix tabs

--- younon/public/js/app.js
+++ younon/public/js/app.js
@@ -38,6 +38,17 @@ angular.module('starter', ['ionic','ngIOS9UIWebViewPatch','me-lazyimg','index.co
 
 .config(function($stateProvider, $urlRouterProvider,$ionicConfigProvider) {
         $ionicConfigProvider.tabs.position('bottom');
+        $ionicConfigProvider.platform.android.tabs.style('standard');
+        $ionicConfigProvider.platform.android.tabs.position('standard');
+
+        $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+        $ionicConfigProvider.platform.android.navBar.alignTitle('left');
+
+        $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+        $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
+
+        $ionicConfigProvider.platform.ios.views.transition('ios');
+        $ionicConfigProvider.platform.android.views.transition('android');
         $stateProvider
       .state('tab', {
     url: '/tab',
