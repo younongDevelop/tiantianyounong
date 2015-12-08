fa429945b03fb25c892592ee0abddb567a79445b
weixiangbin <317505615@qq.com>
2015-12-08 14:23:36 +0800

    fix bug

--- younon/public/js/index/indexCtr.js
+++ younon/public/js/index/indexCtr.js
@@ -6,10 +6,10 @@ var signback = function (data) {
         timestamp: data.timestamp,
         nonceStr: data.nonceStr,
         signature: data.signature,
-        jsApiList: ['chooseWXPay','onMenuShareTimeline', 'onMenuShareAppMessage','onMenuShareQQ','onMenuShareWeibo','onMenuShareQZone','hideMenuItems']
+        jsApiList: ['chooseWXPay','onMenuShareTimeline', 'onMenuShareAppMessage','onMenuShareQQ','onMenuShareWeibo','hideMenuItems']
     });
     wx.ready(function () {
-    
+
     });
     shareIndex();
     wx.error(function (res) {
@@ -92,18 +92,6 @@ var shareIndex = function (param,imageUrl) {
         }
     });
 
-    wx.onMenuShareQZone({
-        title: title, // 分享标题
-        desc: '一个专注于本地农产品的网站', // 分享描述
-        link: link, // 分享链接
-        imgUrl: imgUrl, // 分享图标
-        success: function () {
-            // 用户确认分享后执行的回调函数
-        },
-        cancel: function () {
-            // 用户取消分享后执行的回调函数
-        }
-    });
 
 };
 
